8630c775e17ca91f3876b1b351d773ea2e828375
Tomasz Ludek <tomasz.ludek@gmail.com>
2017-12-19 15:13:43 +0100

    Adding back IntentContract.RequestWalletSeed required by dash module until coin level keys are not supported by dashj

--- src/main/java/com/mycelium/spvmodule/IntentContract.java
+++ src/main/java/com/mycelium/spvmodule/IntentContract.java
@@ -77,6 +77,20 @@ public interface IntentContract {
         }
     }
 
+    class RequestWalletSeed {
+        public static final String ACTION = "com.mycelium.wallet.requestWalletSeed";
+        public static final String BIP39_PASS_PHRASE_EXTRA = ACTION + "_bip39Passphrase";
+        public static final String CREATION_TIME_SECONDS_EXTRA = ACTION + "_creationTimeSeconds";
+
+        public static Intent createIntent(int accountId, ArrayList<String> bip39Passphrase, long creationTimeSeconds) {
+            Intent intent = new Intent(ACTION);
+            intent.putExtra(IntentContract.ACCOUNT_INDEX_EXTRA, accountId);
+            intent.putStringArrayListExtra(BIP39_PASS_PHRASE_EXTRA, bip39Passphrase);
+            intent.putExtra(CREATION_TIME_SECONDS_EXTRA, creationTimeSeconds);
+            return intent;
+        }
+    }
+
     class SetPrivateKeyExtendedKeyCoinType {
         public static final String ACTION = "com.mycelium.wallet.setPrivateKeyExtendedKeyCoinType";
         public static final String PRIVATE_KEY = ACTION + "_data";
