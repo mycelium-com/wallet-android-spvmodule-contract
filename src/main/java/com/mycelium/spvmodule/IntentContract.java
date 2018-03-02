2040e62fc9fd44e8741b4fd6a1cdee84988f6f67
Leo Wandersleb <leo@leowandersleb.de>
2018-03-02 03:35:56 -0300

    minor refactoring
    javadoc instead of comment, inlined a variable

--- src/main/java/com/mycelium/spvmodule/IntentContract.java
+++ src/main/java/com/mycelium/spvmodule/IntentContract.java
@@ -185,13 +185,15 @@ public interface IntentContract {
 
     }
 
-    //This intent should be called when app is first started to ensure that no old data related to old app installations remains in modules.
+    /**
+     * This intent should be called when app is first started to ensure that no old data related to
+     * old app installations remains in modules.
+     */
     class ForceCacheClean {
         public static final String ACTION = "com.mycelium.wallet.forceCacheClean";
 
         public static Intent createIntent() {
-            Intent intent = new Intent(ACTION);
-            return intent;
+            return new Intent(ACTION);
         }
     }
 }
